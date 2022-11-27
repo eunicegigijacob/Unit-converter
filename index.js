9efbe7d705594682d500b365a9e10a887357f678
eunicegigijacob <eunice.gigij@gmail.com>
2022-11-27 01:53:12 +0100

    Add event listener to button in javascript  file.
    Add function to convert length values, and call function using event listener added to the convert button.

--- index.js
+++ index.js
@@ -3,3 +3,25 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const input = document.getElementById('input');
+const convertBtn = document.getElementById('convert-btn');
+const showLength = document.getElementById('length');
+const showVolume = document.getElementById('volume');
+const showMass = document.getElementById('mass');
+
+
+convertBtn.addEventListener('click', () =>{
+    if (input.value != ''){
+        const numberToConvert = Number(input.value)
+        lengthConverter(numberToConvert);
+
+    }
+})
+
+
+const lengthConverter = (num) =>{
+    const lengthToFeet = (num * 3.281).toFixed(3)
+    const lengthToMeter =( num / 3.281).toFixed(3)
+    showLength.innerText = `${num} Meters = ${lengthToFeet} feet | ${num} feet = ${lengthToMeter} meters`
+};
